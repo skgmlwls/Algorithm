3e41b0f45b9540823ff2a6dab23c61b2f7f452aa
skgmlwls <127082621+skgmlwls@users.noreply.github.com>
2025-09-06 04:54:56 +0900

    Update 1_StringReverse.kt

--- StudyCode/1_StringReverse.kt
+++ StudyCode/1_StringReverse.kt
@@ -1,1 +1,26 @@
+import java.util.Scanner
 
+fun main(){
+
+    val scanner = Scanner(System.`in`)
+
+    // 1) 문자열 뒤집기
+    println("문자열 입력 : ")
+    val text = scanner.nextLine()
+    println(solution1(text))
+    println(solution2(text))
+
+
+}
+
+
+fun solution1(s: String): String {
+    val strList = mutableListOf<String>()
+
+    for(i in s.length-1 downTo 0) {
+        strList.add(s[i].toString())
+    }
+
+    return strList.joinToString("")
+}
+fun solution2(s: String): String = s.reversed()
